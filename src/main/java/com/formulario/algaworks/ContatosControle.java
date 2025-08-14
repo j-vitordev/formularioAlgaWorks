b7498b9592cb7c4486d1b177d643b2a430d841f2
Vitor <vitorcostaofficial@gmail.com>
2025-08-13 23:25:43 -0300

    feat criação do static

--- src/main/java/com/formulario/algaworks/ContatosControle.java
+++ src/main/java/com/formulario/algaworks/ContatosControle.java
@@ -3,8 +3,16 @@ package com.formulario.algaworks;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class ContatosControle {
+
+    private static final ArrayList<Pessoa> LISTA_PESSOAS = new ArrayList<>();
+
+    static {
+        LISTA_PESSOAS.add(new Pessoa("Vitor",29));
+    }
 
     @GetMapping("/")
     public String index() {
