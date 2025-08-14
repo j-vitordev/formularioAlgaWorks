47e8fa666fa4db00ccb08378bd49eb5f4c33429f
Vitor <vitorcostaofficial@gmail.com>
2025-08-13 23:26:35 -0300

    feat: criaçÃão da classe pessoa

--- src/main/java/com/formulario/algaworks/Pessoa.java
+++ src/main/java/com/formulario/algaworks/Pessoa.java
@@ -1,4 +1,28 @@
 package com.formulario.algaworks;
 
 public class Pessoa {
+    private String nome;
+    private Integer idade;
+
+
+    public Pessoa(String nome, Integer idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
 }
